aeeac12c966b25f7e903f05f400654d5fb349b30
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2013-06-22 20:04:33 +0000

    Moving mrbs, mycomments and myiframe non english files to XoopsLanguages folder.

--- XoopsModules/mrbs/trunk/mrbs/language/nederlands/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$mydirname = basename(dirname(dirname(dirname( __FILE__ ))));
-$GLOBALS["MRBS_PREFIX"] = strtoupper($mydirname);
-
-
-?>
--- XoopsModules/mrbs/trunk/mrbs/language/turkish_utf8/main.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mydirname = basename(dirname(dirname(dirname( __FILE__ ))));
-$GLOBALS["MRBS_PREFIX"] = strtoupper($mydirname);
-
-?>
